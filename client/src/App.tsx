b367ce4ed4633de8f33e0362fd71d7d27729f1eb
kj-ninja <jakubiak.krzy@gmail.com>
2024-10-20 15:01:49 +0200

    fix: removed other exports from button file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,15 @@ import { Button } from "@/components/ui/Button";
 function App() {
   const { data, refetch } = useUsers();
 
-  console.log(data);
-
   return (
     <>
       <h1>React</h1>
       <div className="card">
         <h1>Hello world!</h1>
+        <ul>{data?.map((user) => <li key={user.id}>{user.id}</li>)}</ul>
       </div>
       <p className="read-the-docs mb-1">POC with react-query and axios</p>
-      <Button onClick={() => refetch()}>Click me</Button>
+      <Button onClick={() => refetch()}>Fetch users</Button>
     </>
   );
 }

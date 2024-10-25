b61a7d4153a76276b70ff36b2027c34827cddfb8
kj-ninja <jakubiak.krzy@gmail.com>
2024-10-25 15:24:35 +0200

    feat: added login rate limiter middleware

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { loginUser, refreshToken, registerUser, validateToken } from "@controllers/auth.controller";
 import passport from "passport";
+import { loginLimiter } from "@middleware/rateLimiter";
 
 const router = Router();
 
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login", loginLimiter, loginUser);
 router.post("/refresh", refreshToken);
 router.get("/auth/validate", passport.authenticate("jwt", { session: false }), validateToken);
 router.get("/protected", passport.authenticate("jwt", { session: false }), (req, res) => {

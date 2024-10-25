3bc9766671653dc83da6c918fd5eb6626ff88122
kj-ninja <jakubiak.krzy@gmail.com>
2024-10-25 18:51:41 +0200

    feat: separate server and app also added rate limiter middleware to login route

---
+++ server/src/middleware/rateLimiter.ts
@@ -0,0 +1,8 @@
+import rateLimit from "express-rate-limit";
+
+export const loginLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  handler: (req, res) => {
+    res.status(429).json({ message: "Too many requests. Please try again later." });
+  },
+});

c1eaf17b5829722de1d5441c660b423bbcfa6f5e
kj-ninja <jakubiak.krzy@gmail.com>
2024-10-27 22:23:14 +0100

    fix: removed unused hook

--- client/src/features/auth/hooks/useUsers.ts
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { AuthQueries } from "@/features/auth/api/queryKeys";
-
-export function useUsers() {
-  return useQuery(AuthQueries.getUsers());
-}
